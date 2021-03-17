9c4f1c8e4db0b855a404be6e45dfbfcf69353043
simplyedwin <simplyedwin@gmail.com>
2021-03-17 07:46:59 +0000

    added code to read imageurl

--- app.py
+++ app.py
@@ -21,24 +21,29 @@ db = client[DB_NAME]
 @app.route('/movies')
 def landing_page():
     all_genre = db.movie_genres.find()
+    all_movies = db.movies.find()
+
+    # files = os.listdir('./images')
+
     # for result in all_genre:
     #     print(result['genre'])
-    return render_template('landingpage.template.html', all_genre=all_genre)
+    return render_template('landingpage.template.html',
+                           all_genre=all_genre,all_movies=all_movies)
 
 
 @app.route('/movies/create')
 def show_create_movie():
-    path = 'images/resident_evil_retribution_poster.jpg'
-    with open(path, 'rb') as f:
-        contents = f.read()
-        binary_file = Binary(contents)
-        db.movie_thumbnails.insert_one({
-            'name': 'RE',
-            'file': binary_file
-        })
+    # with open(path, 'rb') as f:
+    #     contents = f.read()
+    #     binary_file = Binary(contents)
+    #     db.movie_thumbnails.insert_one({
+    #         'name': 'RE',
+    #         'file': binary_file
+    #     })
     return render_template('create_movieinfo.template.html')
 
 
 if __name__ == '__main__':
+    print(os.listdir('./images'))
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'), debug=True)
