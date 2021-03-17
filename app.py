aa684f5c18cd37c3beead586850eeaffc59a6a06
simplyedwin <simplyedwin@gmail.com>
2021-03-17 04:17:34 +0000

    added code to add image file temp sol

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from dotenv import load_dotenv
 import pymongo
+from bson.binary import Binary
 
 # we can use ObjectId
 from bson.objectid import ObjectId
@@ -27,6 +28,14 @@ def landing_page():
 
 @app.route('/movies/create')
 def show_create_movie():
+    path = 'images/resident_evil_retribution_poster.jpg'
+    with open(path, 'rb') as f:
+        contents = f.read()
+        binary_file = Binary(contents)
+        db.movie_thumbnails.insert_one({
+            'name': 'RE',
+            'file': binary_file
+        })
     return render_template('create_movieinfo.template.html')
 
 
